edf690268cd4686a0a10da356c016548de5cdad4
gxq1219 <git@github.com>
2023-08-31 15:09:35 +0800

    first commit

---
+++ ABACUS_Interface.py
@@ -0,0 +1,219 @@
+"""
+USPEX.Stages.ABACUS_Interface
+===========================
+
+"""
+import logging
+import numpy as np
+import shutil
+import os
+from os.path import join as pj
+from typing import List
+from ase.io import write,read
+from .KPoints import KPoints, BadKPoints
+from ase.atoms import Atoms
+import glob
+from ase.io.abacus import AbacusOutCalcChunk, AbacusOutHeaderChunk
+
+logger = logging.getLogger(__name__)
+
+
+class ABACUS_Interface:
+    '''
+    Calculator for ABACUS.
+    Local running
+    '''
+
+
+    inputFile, outputFile, errorFile = 'INPUT', 'output', 'error'
+
+    # working input files
+    input_file = 'INPUT'
+    kpoints_file = 'KPT'
+    stru_file = 'STRU'
+    potentials_file = 'Specific/ATOMIC_SPECIES'
+    orbital_file = 'Specific/NUMERICAL_ORBITAL'
+    sub_file='Specific/run.sh'
+
+    with open("Specific/INPUT_1", "r") as file:
+        lines = file.readlines()
+        suffix_line = None
+        for line in lines:
+            if 'suffix' in line:
+                suffix_line = line
+                break
+        if suffix_line:
+            word = suffix_line.rstrip('\n').split('suffix')[0]
+        else:
+            word = ['USPEX']
+
+    #### Reading potential
+    pseudopotentials = dict()
+
+    with open('Specific/ATOMIC_SPECIES', 'r') as pse:
+        Lines =  pse.readlines()
+        for line in Lines:
+            element = line.rstrip('\n').split()[0]
+            mass = line.rstrip('\n').split()[1]
+            pot = line.rstrip('\n').split()[2]
+            pseudopotentials[element] = pot
+    
+    ### Reading orbital
+    basis = dict()
+    with open('Specific/NUMERICAL_ORBITAL','r') as orb:
+        Lines_orb = orb.readlines()
+        for line_orb in Lines_orb:
+            element_o = line.rstrip('\n').split()[0]
+            Orb = line.rstrip('\n').split()[1]
+            basis[element_o] = Orb
+
+
+    # working output files
+    log1_file = 'OUT.USPEX/running_cell-relax.log'
+    log2_file = 'OUT.USPEX/running_relax.log'
+    log3_file = 'OUT.USPEX/running_scf.log'
+    cif_file = 'OUT.USPEX/STRU_NOW.cif'
+
+    DEFAULT_SLEEP_TIME = 30
+
+    aseAdapterType = None
+
+    #print(pseudopotentials)
+    @classmethod
+    def registerTypes(cls, aseAdapterType):
+        cls.aseAdapterType = aseAdapterType
+
+    def __init__(self, tag: str, kresol: float, input: str = None, pseudopotentials = pseudopotentials, basis =  basis ,targetProperties: list = None, **kwargs):
+        '''
+        :param params: dictionary with parameters:
+                * commandExecutable: str of executable command
+                * kresol: float of K-points resolution
+                * remote: dict of remote server params
+                * taskManager: dict of task managers params
+        :param step: int of current step
+        '''
+        self.tag = tag
+        if input is not None:
+            self.input = input
+        else:
+            self.input = pj(os.getcwd(), f'Specific/INPUT_{tag}')
+        assert os.path.exists(self.input)
+        self.output_file='OUT.USPEX'
+        self.sub_file='Specific/run.sh'
+        self.log_file1='OUT.USPEX/running_cell-relax.log'
+        self.log_file2='OUT.USPEX/running_relax.log'
+        self.log_file3='OUT.USPEX/running_scf.log'
+        #print(pseudopotentials)
+        self.adapter = self.aseAdapterType()
+        self.kPoints = KPoints(kresol)
+        #pseudopotentials_path = 'Specific/'
+        self.pseudopotentials = {s : p for s, p in pseudopotentials.items()}
+        #print(self.input)
+
+        #for x, p in self.pseudopotentials.items():
+        #    assert p.exists()
+        if 'lcao' not in self.input:
+            self.basis = None
+        else:
+            self.basis = {s: orbital for s, orbital in basis.items()}
+            for x, orbital in self.basis.items():
+                assert orbital.exists()
+        self.failedSystems = []
+        self.targetProperties = targetProperties if targetProperties is not None else ['structure', 'enthalpy']
+
+
+    def prepareLocalCalculation(self, system, calcFolder: str):
+        '''
+        :param system: our system
+        :return:
+        '''
+        with open(pj(calcFolder, self.input_file), 'wt') as f:
+            pass
+       
+        structure = system['structure']
+
+        with open('Specific/ATOMIC_SPECIES', 'r') as pse:
+             Lines =  pse.readlines()
+             for line in Lines:
+                 upf = line.rstrip('\n').split()[2]
+                 source_file = 'Specific' + "/" + upf
+                 shutil.copy(source_file, calcFolder)
+
+
+        ############################## INPUT ################################
+        source = self.input 
+        dest =  pj(calcFolder, self.input_file)
+        shutil.copy2(source, dest)
+
+        if system['externalPressure']:
+            with open(pj(calcFolder, self.input_file), 'a') as myfile:
+                myfile.write(f"press1 \t {10*system['externalPressure']:10f}\n")
+                myfile.write(f"press2 \t {10*system['externalPressure']:10f}\n")
+                myfile.write(f"press3 \t {10*system['externalPressure']:10f}\n")
+        if calcFolder in self.failedSystems:
+            with open(pj(calcFolder, self.input_file), 'a') as myfile: myfile.write('symmetry -1\n')
+        ############################# KPT #################################
+        try:
+            kPoints = self.kPoints.build(structure.getCell())
+        except BadKPoints:
+            # This LATTICE is extremely wrong, let's skip it from now
+            logger.info('K-points cannot be built, so it\'s set as   [1, 1, 1]')
+            kPoints = [1, 1, 1]
+
+        with open(pj(calcFolder, self.kpoints_file), 'w') as fp:
+            fp.write('K_POINTS\n0\nGamma\n')
+            fp.write('%4d %4d %4d   0   0   0\n' % tuple(kPoints))
+
+    ############################# STRU ##################################
+        #system['ase'] = self.adapter.write( structure, self.pseudopotentials, self.basis, calcFolder)
+        cell = structure.getCell()
+        symbols = np.asarray([el.short_name for el in structure.getAtomTypes()])
+        order = np.argsort(symbols)
+        atoms = Atoms(symbols[order], structure.getCartesianCoordinates()[order], cell=cell.getCellVectors())
+
+        if self.pseudopotentials is not None:
+            pse = {element : poten for element, poten in self.pseudopotentials.items() if element in set(symbols)}
+        else:
+            pse = None
+
+        if self.basis is not None:
+            bas = {element : basi for elment, basi in self.basis.items() if element in set(symbols)}
+        else:
+            bas = None
+        system['ase']=self.adapter.write(pj(calcFolder,self.stru_file), structure, pp = pse, basis = bas)
+
+        return ''
+
+    # Checking whether the SCF has converged
+    def isConverged(self, calcFolder: str):
+        filename  = pj(calcFolder, self.output_file)
+        for i in os.listdir(filename):
+            if i.startswith('running') and i.endswith('log'):
+                log = i
+                with open(pj(filename,log), 'r') as file:
+                    content = file.read()
+                    header = AbacusOutHeaderChunk(content)
+                    result= AbacusOutCalcChunk(content,header=header)
+                break
+            else:
+                logger.info('No log file.')
+ 
+        return result
+
+############   Read
+    def readOutput(self, system: dict, calcFolder: str):
+        filename=pj(calcFolder, self.output_file)
+        aseResults = self.adapter.read(filename, **system.pop('ase'))
+        results = {}
+        logger.info(f"aseResults['results'].results.keys()")
+        if 'structure' in self.targetProperties:
+            results['structure'] = aseResults['structure']
+        if 'enthalpy' in self.targetProperties:
+            volume = aseResults['volume']
+            results['enthalpy'] = aseResults['results'].results['energy'] + system['externalPressure'] * volume
+        if 'energy' in self.targetProperties:
+            results['energy'] = aseResults['results'].results['energy']
+        if 'forces' in self.targetProperties:
+            results['forces'] = aseResults['results'].results['forces']
+        
+        return results 
---
+++ ASEInterfaceAdapter.py
@@ -0,0 +1,192 @@
+import numpy as np
+from os.path import join as pj, exists as ex
+from ase.io.vasp import iread_vasp_out, read_vasp_xml, write_vasp
+from ase.io.espresso import read_fortran_namelist, read_espresso_out, write_espresso_in
+from ase.io.abacus import write_abacus, read_abacus_results, read_abacus_out
+from ase.io import ParseError, read, write
+from ase.atoms import Atoms
+from ase.constraints import FixAtoms
+import os
+
+class ASEInterfaceAdapter:
+    structureType = None
+    atomType = None
+    cellType = None
+
+    @classmethod
+    def registerTypes(cls, structureType, atomType, cellType):
+        cls.structureType = structureType
+        cls.atomType = atomType
+        cls.cellType = cellType
+
+    class Results:
+
+        EV_PER_CUBIC_ANGSTREM_PER_GPA = 1 / 160.21766208
+
+        def __init__(self, atoms):
+            self.results = atoms.get_calculator().results
+            self.volume = atoms.get_volume()
+
+        def getEnthalpy(self, pressure):
+            return self.results['enthalpy'] if 'enthalpy' in self.results else \
+                self.results['energy'] + self.volume * pressure * self.EV_PER_CUBIC_ANGSTREM_PER_GPA
+
+    class VASP:
+
+        # VASP output files
+        outcar_file = 'OUTCAR'
+        oszicar_file = 'OSZICAR'
+        contcar_file = 'CONTCAR'
+        xml_file = 'vasprun.xml'
+
+        # VASP input files
+        incar_file = 'INCAR'
+        kpoints_file = 'KPOINTS'
+        poscar_file = 'POSCAR'
+        potcar_file = 'POTCAR'
+
+        def write(self, structure, fixedIndices, label, calcFolder):
+            cell = structure.getCell()
+            symbols = np.asarray([el.short_name for el in structure.getAtomTypes()])
+            order = np.argsort(symbols)
+            atoms = Atoms(symbols[order], structure.getCartesianCoordinates()[order], cell=cell.getCellVectors())
+            if len(fixedIndices) > 0:
+                atoms.set_constraint(FixAtoms(indices=fixedIndices))
+            write_vasp(pj(calcFolder, self.poscar_file), atoms, label=label, direct=True, vasp5=True, long_format=False)
+            return {'pbc': cell.getPBC(), 'symbolsOrder': order}
+
+        def read(self, calcFolder, pbc, symbolsOrder):
+            try:
+                with open(pj(calcFolder, self.outcar_file)) as f:
+                    trajectoryAtoms = list(iread_vasp_out(f, None))
+            except (KeyError, ParseError):
+                with open(pj(calcFolder, self.xml_file)) as f:
+                    trajectoryAtoms = list(read_vasp_xml(f))
+            trajectory = []
+            for atoms in trajectoryAtoms:
+                size = len(atoms)
+                positions = np.empty((size, 3), dtype=float)
+                atomTypes = np.empty(size, dtype=ASEInterfaceAdapter.atomType)
+                for i, symbol, position in zip(symbolsOrder, atoms.get_chemical_symbols(), atoms.get_positions()):
+                    positions[i] = position
+                    atomTypes[i] = ASEInterfaceAdapter.atomType(symbol)
+                cell = ASEInterfaceAdapter.cellType(atoms.get_cell().array, pbc)
+                structure = ASEInterfaceAdapter.structureType(atomTypes, positions, cell=cell)
+                trajectory.append(dict(
+                    structure=structure,
+                    results=ASEInterfaceAdapter.Results(atoms)
+                ))
+            return trajectory
+
+    class LAMMPS:
+
+        # LAMMPS files
+        data_file = 'STRUC'
+        dump_file = 'lammps.dump'
+
+        def write(self, structure, fixedIndices, label, specorder, calcFolder):
+            cell = structure.getCell()
+            atoms = Atoms([el.short_name for el in structure.getAtomTypes()], structure.getCartesianCoordinates(),
+                          cell=cell.getCellVectors())
+            filename = pj(calcFolder, self.data_file)
+            atoms.write(filename, format='lammps-data', specorder=specorder)
+            with open(filename, 'rt') as f:
+                content = f.readlines()
+            content[0] = f'{label}\n'
+            with open(filename, 'wt') as f:
+                f.writelines(content)
+            return {'pbc': cell.getPBC()}
+
+        def read(self, calcFolder, specorder, pbc):
+            if ex(pj(calcFolder, self.dump_file)):
+                atoms = read(pj(calcFolder, self.dump_file), format='lammps-dump-text')
+                atomTypes = np.array(
+                    [ASEInterfaceAdapter.atomType(specorder[i - 1]) for i in atoms.get_atomic_numbers()])
+                structure = ASEInterfaceAdapter.structureType(atomTypes, atoms.get_positions(),
+                                                              cell=ASEInterfaceAdapter.cellType(atoms.get_cell().array,
+                                                                                                pbc))
+                return dict(
+                    structure=structure,
+                    results=ASEInterfaceAdapter.Results(atoms)
+                )
+            else:
+                return None
+
+    class QE:
+
+        inputFile, outputFile = 'input', 'output'
+
+        def __init__(self, options):
+            with open(options) as fp:
+                data, card_lines = read_fortran_namelist(fp)
+            if 'system' not in data:
+                raise KeyError('Required section &SYSTEM not found.')
+            self.data = data
+
+        def write(self, structure, fixedIndices, kPoints, pseudopotentials, calcFolder):
+            cell = structure.getCell()
+            atoms = Atoms(symbols=[el.short_name for el in structure.getAtomTypes()],
+                          positions=structure.getCartesianCoordinates(),
+                          cell=cell.getCellVectors())
+            if len(fixedIndices) > 0:
+                atoms.set_constraint(FixAtoms(indices=fixedIndices))
+            with open(calcFolder / self.inputFile, 'wt') as f:
+                write_espresso_in(f,
+                                  atoms=atoms, input_data=self.data,
+                                  pseudopotentials={s: p.name for s, p in pseudopotentials.items()},
+                                  kpts=kPoints,
+                                  crystal_coordinates=True)
+            return {'pbc': cell.getPBC()}
+
+        def read(self, calcFolder, pbc):
+            with open(pj(calcFolder, self.outputFile)) as f:
+                atoms = next(read_espresso_out(f, index=slice(None, -2, -1)))
+            atomTypes = np.array([ASEInterfaceAdapter.atomType(s) for s in atoms.get_chemical_symbols()])
+            structure = ASEInterfaceAdapter.structureType(atomTypes, atoms.get_positions(),
+                                                          cell=ASEInterfaceAdapter.cellType(atoms.get_cell().array,
+                                                                                            pbc))
+            return dict(
+                structure=structure,
+                results=ASEInterfaceAdapter.Results(atoms)
+            )
+
+    class ABACUS:
+        # ABACUS input files
+        input_file = 'INPUT'
+        kpoints_file = 'KPT'
+        stru_file = 'STRU'
+
+        # ABACUS output files
+        cif_file = 'OUT.USPEX/STRU_NOW.cif'
+        log1_file = 'OUT.USPEX/running_cell-relax.log'
+        log2_file = 'OUT.USPEX/running_relax.log'
+        log3_file = 'OUT.USPEX/running_scf.log'
+        output_file = 'OUT.USPEX'        
+
+        def write(self, filename, structure, pp, basis):
+            cell = structure.getCell()
+            symbols = np.asarray([el.short_name for el in structure.getAtomTypes()])
+            order = np.argsort(symbols)
+            atoms = Atoms(symbols[order], structure.getCartesianCoordinates()[order], cell=cell.getCellVectors())
+            #if len(fixedIndices) > 0:
+            #    atoms.set_constrain(FixAtoms(indices=fixedIndices))
+            with open(filename, 'wt') as f:
+                write_abacus(f, atoms, pp, basis)
+            
+            return {'pbc': cell.getPBC()}
+
+        def read(self, foldername, pbc):
+            for i in os.listdir(foldername):
+               if i.startswith('running') and i.endswith('log'):
+                   log = i
+                   break
+               else:
+                   logger.info('No log file.')
+            atoms = read(pj(foldername,log), format='abacus-out')
+            atomTypes = np.array([ASEInterfaceAdapter.atomType(s) for s in atoms.get_chemical_symbols()])
+            structure = ASEInterfaceAdapter.structureType(atomTypes, atoms.get_positions(), \
+                        cell=ASEInterfaceAdapter.cellType(atoms.get_cell().array, pbc))
+            volumeline = os.popen('grep Volume {} |tail -n 1'.format(pj(foldername,log))).read()
+            volume = volumeline.strip().split('=')[1]
+            return dict(structure=structure, results=ASEInterfaceAdapter.Results(atoms), volume = float(volume))
+            
